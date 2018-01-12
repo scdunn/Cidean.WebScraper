057ad72865b93f4d439380702f0799590afcd369
Chris Dunn <chris@cidean.com>
2018-01-11 23:24:56 -0500

    Welcome messaging and console scaffolding.

--- Cidean.WebScraper/Program.cs
+++ Cidean.WebScraper/Program.cs
@@ -8,8 +8,29 @@ namespace Cidean.WebScraper
 {
     class Program
     {
+
+        //text divider for a line in console ie '============='
+        static string LineDivider = new string('=', 120);
+
+
+        /// <summary>
+        /// main console start
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Cidean's WebScraper.");
+            Console.WriteLine(LineDivider);
+            Console.WriteLine("Please use this application responsibly and respect all copyrighted material.");
+
+            //Todo: BEGIN INPUT FOR PROCESSING
+
+            //Exit Application
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+   
+   
         }
+        
     }
 }
