51634d81f8c8ab3edef7030ee0f52bc897205a49
Chris Dunn <chris@cidean.com>
2018-01-16 11:23:00 -0500

    Add basic DataMap for configuration

---
+++ src/DataMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cidean.WebScraper
+{
+    /// <summary>
+    /// Definition file for content to scrape and output configuration
+    /// </summary>
+    public class DataMap
+    {
+        public string Name { get; set; }
+        public List<string> Urls { get; set; }
+
+        
+
+    }
+}
