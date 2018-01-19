de22b10acfdb3d17dab8549c09e26f478bc0de01
Chris Dunn <chris@cidean.com>
2018-01-18 21:40:07 -0500

    Add Xml Serialization/Deserialize to datamap from loadfile

--- src/DataMap.cs
+++ src/DataMap.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Cidean.WebScraper
 {
@@ -11,10 +14,39 @@ namespace Cidean.WebScraper
     /// </summary>
     public class DataMap
     {
+        [XmlElement("Name")]
         public string Name { get; set; }
+        [XmlArray]
+        [XmlArrayItem(ElementName = "Url")]
         public List<string> Urls { get; set; }
 
-        
+        /// <summary>
+        /// Load datamap xml file and deserialize into object.  Static method on class.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns>DataMap</returns>
+        public static DataMap LoadFile(string filename)
+        {
+            //return value
+            DataMap map;
+
+            //create serializer
+            XmlSerializer serializer = new XmlSerializer(typeof(DataMap));
+
+            //open filestream and create reader
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                XmlReader reader = XmlReader.Create(fs);
+
+                //deserialize to datamap object and close filestream
+                map = (DataMap)serializer.Deserialize(reader);
+                fs.Close();
+            }
+                
+            //return result map
+            return map;
+        }
+
 
     }
 }
