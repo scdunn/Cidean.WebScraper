39bf1046c366c4eac67fc6c6b20f798610f44490
Chris Dunn <chris@cidean.com>
2018-03-06 14:09:39 -0500

    Add download image function to support image scraping

--- src/Cidean.WebScraper.Core/Scraper.cs
+++ src/Cidean.WebScraper.Core/Scraper.cs
@@ -252,6 +252,56 @@ namespace Cidean.WebScraper.Core
             return elements.ToList();
         }
 
-        
+
+        /// <summary>
+        /// Download Image from remote location
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="fileName"></param>
+        private bool DownloadImage(string uri, string fileName)
+        {
+
+            var url = uri;
+            
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Check that the remote file was found. The ContentType
+                // check is performed since a request for a non-existent
+                // image file might be redirected to a 404-page, which would
+                // yield the StatusCode "OK", even though the image was not
+                // found.
+                if ((response.StatusCode == HttpStatusCode.OK ||
+                    response.StatusCode == HttpStatusCode.Moved ||
+                    response.StatusCode == HttpStatusCode.Redirect) &&
+                    response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+                {
+
+                    // if the remote file was found, download oit
+                    using (Stream inputStream = response.GetResponseStream())
+                    using (Stream outputStream = File.OpenWrite(fileName))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                            outputStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead != 0);
+                    }
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                LogEvent(ex.Message + "  " + url);
+                return false;
+            }
+
+        }
+
     }
 }
